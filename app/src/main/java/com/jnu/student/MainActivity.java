504c600c8d9582cfc26d84a33f061297318aaa81
nancheng233 <ljl2021102235@outlook.com>
2023-09-21 10:34:11 +0800

    lab3

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,8 +1,9 @@
 package com.jnu.student;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView Hello_Android = findViewById(R.id.text_view_hello_world);
+        Hello_Android.setText(R.string.Hello_Android);
     }
 }
